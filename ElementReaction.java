434482952873b9c3b295cebe9e588ee5a8cc50a9
coco-samiyar <59204573+kiliti-corder@users.noreply.github.com>
2024-10-17 17:46:05 +0800

    Add files via upload

---
+++ ElementReaction.java
@@ -0,0 +1,33 @@
+public class ElementReaction {
+    /**
+     * element seven
+     *      Pyro (火元素) 🔥
+     *      Hydro (水元素) 💧
+     *      Anemo (风元素) 🌬
+     *      Electro (雷元素) ⚡
+     *      Dendro (草元素) 🌿
+     *      Cryo (冰元素) ❄
+     *      Geo (岩元素) 🗻
+     *
+     * reaction
+     *    Pyro + Hydro => Vaporize (蒸发)
+     *    Pyro + Cryo => Melt (融化)
+     *    Pyro + Electro => Overloaded (超载)
+     *    Electro + Cryo => Superconduct (超导)
+     *    Electro + Hydro => Electro-Charged (感电)
+     *    Hydro + Cryo => Frozen (冻结)
+     *    Pyro + Dendro => Burning (燃烧)
+     *    Anemo + (Pyro/Hydro/Electro/Cryo) => Swirl (扩散)
+     *    Geo + (Pyro/Hydro/Electro/Cryo) =>  Crystallize (结晶)
+     *    Hydro + Dendro => Bloom(绽放)[creates Dendro Cores,exploded Dendro Cores(草原核) deals AoE Dendro DMG]
+     *    Bloom + Electro => Hyperbloom(超绽放)[Dendro Cores transform into homing Sprawling Sprawling Shots(蔓生弹)]
+     *    Bloom + Pyro => Burgeon(烈绽放)[causes greater AoE Dendro DMG]
+     *    Dendro + Electro => Quicken(原激化)
+     *    Quicken + Dendro => Spread(蔓激化)
+     *    Quicken + Electro => Aggravate(超激化)
+     *
+     *
+     *
+     */
+
+}
